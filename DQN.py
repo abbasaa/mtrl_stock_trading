9983ba59ba6fd175f4e97ee1ee79ed8033537aa1
Abbas Ahmed <abbasaa@umich.edu>
2021-04-23 21:27:32 -0400

    updated dqn to load in and freeze pricingnet

--- DQN.py
+++ DQN.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -11,6 +13,27 @@ class DQN(nn.Module):
     def __init__(self, input_size, hidden_dim, output_size, ticker, device):
         super(DQN, self).__init__()
         self.pricing = PricingNet(ticker, device)
+        for param in self.pricing.parameters():
+            param.requires_grad = False
+        # load in pretrained pricingnet
+        checkpoints_dir = os.path.join(os.curdir, 'checkpoints', 'pricingnet', ticker)
+        if not os.path.isdir(checkpoints_dir):
+            raise Exception('No checkpoints dir to load pretrained pricingnet')
+        checkpoint_files = [f for f in os.listdir(checkpoints_dir) if os.path.isfile(os.path.join(checkpoints_dir, f))]
+        if len(checkpoint_files) == 0:
+            raise Exception('No checkpoint files to load pretrained pricingnet')
+
+        max_epoch = -2**32
+        for file in checkpoint_files:
+            cur_epoch = int(file.split('.')[1])
+            if cur_epoch > max_epoch:
+                max_epoch = cur_epoch
+
+        print(f'Loading pricingnet from checkpoint at epoch: {max_epoch}')
+        checkpoint_file = os.path.join(checkpoints_dir, f'pricingnet.{max_epoch}.pth')
+        checkpoint = torch.load(checkpoint_file)
+        self.pricing.load_state_dict(checkpoint['pricingnet_state_dict'])
+
         self.input_size = input_size
         self.hidden_dim = hidden_dim
         self.output_size = output_size
