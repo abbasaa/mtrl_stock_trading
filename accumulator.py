8b67f44eba93487be00e3d9d67dd2e2e4b8678c0
waree123 <35786192+waree123@users.noreply.github.com>
2021-04-25 21:53:55 -0400

    Update accumulator.py

--- accumulator.py
+++ accumulator.py
@@ -1,18 +1,42 @@
 import numpy as np
+import torch
+import torch.nn.functional as F
 
-def accumulator(sector, stock_prices, HP_SA):
+
+
+def step_SA(policy_net, pos, obs, t_step):
+    with torch.no_grad():
+        return policy_net(pos, [t_step], obs[:, -1, 0]).max(1)[1].view(1, 1).float(), policy_net(pos, [t_step], obs[:, -1, 0])
+
+
+def accumulator(sector, stock_prices, test_stock_prices, values):
     # stock prices as MxN (M = # of single agent stocks)
+    # test stock prices as KxN (K = # of test stocks)
     # sector prices as 1xN
-    # HP_SA as MxX
+    # HP_SA as Nx2xK
+
     M = stock_prices.shape[0]
     N = stock_prices.shape[1]
+    K = test_stock_prices.shape[0]
 
-    # calculate correlation coefficients
+    # calculate correlation coefficients for agents
     corr_coef = np.zeros((M,1))
     for i in range(M):
         corr_coef[i,0] = np.corrcoef(stock_prices[i,:], sector)[0,1]
     
-    # calculate hyperparameters
-    HP_A = np.dot(corr_coef.T, HP_SA)/(N * np.sum(corr_coef))
+    # calculate correlation coefficients for test stocks
+    corr_coef_test = np.zeros((K,1))
+    for i in range(K):
+        corr_coef_test[i,0] = np.corrcoef(test_stock_prices[i,:], sector)[0,1]
     
+    # calculate action values for test stocks
+    N_values = values.shape[0]
+    print(N_values)
+    corr_sum = np.zeros((N_values,2,K))
+    for i in range(M):
+        corr_sum = values[:,:,i,:] * corr_coef[i,0] + corr_sum
+        
+    HP_A = np.zeros((N_values,2,K))
+    for i in range(K):
+        HP_A[:,:,i] = corr_coef_test[i,0] * corr_sum[:,:,i]/(np.sum(corr_coef,0))
     return HP_A
